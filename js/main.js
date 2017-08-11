095f3d468c7b76fe7d1dd7e6a5cf01e46be58c7e
josephwagner <joe.wagner@infosys.com>
2017-08-11 10:29:21 -0400

    Add google maps error handling

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-function init() {
+function init(error) {
 
-    // Create the map
-    var map = initMap();
+    var map = null;
 
+    if (!error) {
+        // Create the map
+        map = initMap();
+
+    } else {
+        console.log($);
+        alert('Error loading Google Map. Please reload the page and try again.');
+    }
     // Create the ViewModel
     var vM = new ViewModel(placeList, map);
     ko.applyBindings(vM);
@@ -35,10 +42,12 @@ var ViewModel = function (placeList, map) {
     };
 
     self.placeList().forEach(function (place) {
-        place.marker = new google.maps.Marker({
-            position: place.location(),
-            title: place.name()
-        });
+        if (map !== null) {
+            place.marker = new google.maps.Marker({
+                position: place.location(),
+                title: place.name()
+            });
+        }
     });
 
     // Place the markers on the map
@@ -59,7 +68,10 @@ var ViewModel = function (placeList, map) {
         });
     };
 
-    self.placeMarkers(self.placeList());
+    if (map !== null) {
+        self.placeMarkers(self.placeList());
+        self.addEvents();
+    }
 
     // All places start as visible
     self.visiblePlaceList = ko.observableArray(self.placeList());
@@ -98,8 +110,6 @@ var ViewModel = function (placeList, map) {
     self.highlightedPlaceMenuLink = ko.observable();
     self.highlightedPlaceVenueLink = ko.observable();
 
-    self.addEvents();
-
     self.setHighlightedPlace = function (place) {
 
         // Stop marker bouncing
@@ -118,12 +128,14 @@ var ViewModel = function (placeList, map) {
             document.getElementById('placeInfo').classList.remove('hidden');
 
             // Recenter map, then move up to make room for div sliding from bottom
-K            map.panTo(self.highlightedPlace().marker.getPosition());
+            map.panTo(self.highlightedPlace().marker.getPosition());
 
             self.getPlaceInfo(place);
 
         }
     };
+
+    self.apiSuccess = ko.observable(false);
 
     self.getPlaceInfo = function (place) {
 
@@ -136,12 +148,10 @@ K            map.panTo(self.highlightedPlace().marker.getPosition());
         '&v=20170731' +
         '&limit=1';
 
-        // In case anything goes wrong
-        var errorString = '<p>Unable to retrieve location information for ' + self.highlightedPlace().name() + '.</p>';
-
         $.ajax(url, {
             success: function (data) {
                 if (data.response.venues.length > 0) {
+                    self.apiSuccess(true);
                     var venue = data.response.venues[0];
                     self.highlightedPlaceName(venue.name ? venue.name : 'No address found');
                     self.highlightedPlaceAddress(venue.location.address ? venue.location.address : 'No address available');
@@ -151,11 +161,11 @@ K            map.panTo(self.highlightedPlace().marker.getPosition());
                     self.highlightedPlaceMenuLink(venue.hasMenu ?  venue.menu.url : false);
                     self.highlightedPlaceVenueLink('https://foursquare.com/v/' + venue.id);
                 } else {
-                    document.getElementById('placeInfo').innerHTML = errorString;
+                    self.apiSuccess(false);
                 }
             },
             error: function () {
-                document.getElementById('placeInfo').innerHTML = errorString;
+                self.apiSuccess(false);
             }
         });
     };
@@ -165,7 +175,7 @@ K            map.panTo(self.highlightedPlace().marker.getPosition());
         document.getElementById('placeInfo').classList.add('hidden');
         self.highlightedPlace("");
 
-        map.setCenter(getCenter(self.placeList()));
+        map.panTo(getCenter(self.placeList()));
 
     };
 };
